0dc133793f275e6e565da1f8f43a9475bb8b8c5b
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-07-02 13:50:42 +0700

    add daftar state, akun, pic, peserta registrasi

--- src/views/dashboards/admin/state/DaftarPIC/DaftarPIC.tsx
+++ src/views/dashboards/admin/state/DaftarPIC/DaftarPIC.tsx
@@ -1,20 +1,179 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Spacer,
+  Image,
+  Divider,
+  Center,
+  Button,
+  HStack,
+  CloseButton,
+  Text,
+} from "@chakra-ui/react";
+import { InfoOutlineIcon, EditIcon } from '@chakra-ui/icons'
 import {
   MxmContainers,
   MxmVerticalAlign,
 } from "../../../../../shared/styled/containers";
 import { motion, AnimatePresence } from "framer-motion";
 import { Palette } from "../../../../../types/enums";
+import { MxmLogo } from "../../../../../assets";
 import "./DaftarPIC.scss";
-import { DashboardNavigation } from "../../../../../shared/component/DashboardNavigation";
+import MUIDataTable from 'mui-datatables'
 
 const DaftarPIC: React.FC = () => {
+  const tableColumns = [
+    {
+      name: 'name',
+      label: 'Nama PIC',
+      options: {
+        filter: true,
+        sort: true,
+        customHeadLabelRender: ({index, ...column}) => (
+          <Text key={index} fontWeight="bold" fontFamily="Rubik" fontSize="1.1em">
+            {column.label}
+          </Text>
+        ),
+        setCellProps: () => ({ style: { minWidth: "300px", maxWidth: "300px" }}),
+      },
+    },
+    {
+      name: 'nim',
+      label: 'NIM',
+      options: {
+        filter: true,
+        sort: true,
+        customHeadLabelRender: ({index, ...column}) => (
+          <Text key={index} fontWeight="bold" fontFamily="Rubik" fontSize="1.1em">
+            {column.label}
+          </Text>
+        ),
+        setCellProps: () => ({ style: { minWidth: "250px", maxWidth: "250px" }}),
+      },
+    },
+    
+    {
+      name: 'nama_state',
+      label: 'Nama STATE',
+      options: {
+        filter: true,
+        sort: true,
+        customHeadLabelRender: ({index, ...column}) => (
+          <Text key={index} fontWeight="bold" fontFamily="Rubik" fontSize="1.1em">
+            {column.label}
+          </Text>
+        ),
+        setCellProps: () => ({ style: { minWidth: "250px", maxWidth: "250px" }}),
+      },
+    },
+  ];
+
+  const data = [
+    ["Jane Cooper", "34242", "Ultimagz"],
+    ["Jane Cooper", "23231", "J-Cafe Cosplay"],
+    ["Jane Cooper", "12121", "Ultima Sonora"],
+    ["Jane Cooper", "56565", "Teater Katak"],
+    ["Jane Cooper", "35353", "Game Development Club"],
+  ];
+
   return (
     <>
-      <DashboardNavigation />
+      <Flex
+        height={{
+          base: "100vh",
+          sm: "100vh",
+          md: "80vh",
+          lg: "80vh",
+          xl: "80vh",
+        }}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex
+          direction="column"
+          background="lightgray"
+          py={{
+            base: "3vh",
+            sm: "3vh",
+            md: "3vh",
+            lg: "3vh",
+            xl: "3vh",
+          }}
+          px={{
+            base: "5vw",
+            sm: "5vw",
+            md: "2vw",
+            lg: "2vw",
+            xl: "2vw",
+          }}
+          my={{
+            base: "1vh",
+            sm: "1vh",
+            md: "10vh",
+            lg: "10vh",
+            xl: "10vh",
+          }}
+          mx={{
+            base: "1vw",
+            sm: "1vw",
+            md: "10vw",
+            lg: "10vw",
+            xl: "10vw",
+          }}
+          rounded={20}
+        >
+          <form>
+            <Flex>
+              <Heading
+                mb={3}
+                color="black"
+                letterSpacing="0.05em"
+                fontSize={{
+                  base: "1.5em",
+                  sm: "1.5em",
+                  md: "1.5em",
+                  lg: "1.5em",
+                  xl: "1.7em",
+                  "2xl": "1.7em",
+                }}
+              >
+                Daftar PIC
+              </Heading>
+              <Spacer />
+              <Image
+                src={MxmLogo}
+                alt="Logo MAXIMA 2021"
+                h="100%"
+                w={{
+                  base: "4vw",
+                  sm: "4vw",
+                  md: "2.5vw",
+                  lg: "2vw",
+                  xl: "2vw",
+                  "2xl": "1.2vw",
+                }}
+                mt={2}
+              />
+            </Flex>
+            <Divider
+              colorScheme="blackAlpha"
+              style={{ border: "2px solid black" }}
+            />
+            <Center>
+            <MUIDataTable 
+              data={data} 
+              columns={tableColumns} 
+              options={{
+                selectableRows: false
+              }}
+            />
+            </Center>
+          </form>
+        </Flex>
+      </Flex>
     </>
   );
 };
