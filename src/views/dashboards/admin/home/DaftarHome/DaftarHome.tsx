726f8568615d11688a0340bf2a0ca564691188e7
adrianfinantyo <69502676+adrianfinantyo@users.noreply.github.com>
2021-07-09 12:09:33 +0700

    add dashboard home template

---
+++ src/views/dashboards/admin/home/DaftarHome/DaftarHome.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const DaftarHome: React.FC = () => {
+  return <></>;
+};
+
+export default DaftarHome;
---
+++ src/views/dashboards/admin/home/TambahMedia/TambahMedia.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const TambahMedia: React.FC = () => {
+  return <></>;
+};
+
+export default TambahMedia;
