8eec0513e965e2601f8282abc1afa79514ded371
laitsky <vincent.diamond15@gmail.com>
2021-08-28 16:54:25 +0700

    fix: column name on state detail

--- src/views/dashboards/admin/state/StateDetail/StateDetail.tsx
+++ src/views/dashboards/admin/state/StateDetail/StateDetail.tsx
@@ -93,7 +93,7 @@ const StateDetail: React.FC = () => {
 
   const tableColumns = [
     {
-      name: "name",
+      name: "mahasiswa",
       label: "Nama Mahasiswa",
       options: {
         filter: true,
