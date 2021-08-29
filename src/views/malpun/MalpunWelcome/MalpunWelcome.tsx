eefb4f95e6585f9b95b5272884663c7527300525
TepanD <stefanwork12@gmail.com>
2021-08-29 21:27:59 +0700

    Add: malpunWelcome page
    - Added png assets malpun_maxi and malpun_xima
    - Added scss for MalpunWelcome
    - Added MalpunWelcome page

--- src/views/malpun/MalpunWelcome/MalpunWelcome.tsx
+++ src/views/malpun/MalpunWelcome/MalpunWelcome.tsx
@@ -1,10 +1,63 @@
 import React, { useEffect } from "react";
-import { Flex, Image, Text, Box, createIcon } from "@chakra-ui/react";
+import {
+  Flex,
+  Image,
+  Text,
+  Box,
+  createIcon,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import { Link, useHistory } from "react-router-dom";
+import { motion } from "framer-motion";
+import { MxmButton } from "../../../shared/styled/buttons";
+import { MalpunMaxi, MalpunXima } from "../../../assets/malpun";
+import { Maxi, Xima } from "../../../assets/home";
+import "./MalpunWelcome.scss";
+
+const transition = {
+  duration: 0.5,
+  ease: [0.43, 0.13, 0.23, 0.96],
+};
+
+const cardVariants = {
+  exit: { y: -100, opacity: 0, transition: { delay: 0.2, ...transition } },
+  rest: { y: 100, opacity: 0 },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: 0.2, ...transition },
+  },
+};
+
+const buttonVariantsTwo = {
+  rest: { y: 100, opacity: 0, transition },
+  enter: { y: 0, opacity: 1, transition: { delay: 0.2, ...transition } },
+  exit: { y: 100, opacity: 0, transition: { delay: 0.2, ...transition } },
+};
+
+const frameVariants = {
+  rest: { opacity: 0 },
+  enter: { opacity: 1, transition: { delay: 0, ...transition } },
+  exit: { opacity: 0, transition: { delay: 0.2, ...transition } },
+};
+
+const footerVariants = {
+  exit: { y: "50%", opacity: 0, transition: { delay: 0.2, ...transition } },
+  enter: {
+    y: "0%",
+    opacity: 1,
+    transition,
+  },
+};
 
 const MalpunWelcome = () => {
+  const history = useHistory();
   useEffect(() => {
     document.title = "Malam Puncak 2021";
   }, []);
+
+  const [isShorterThan800px] = useMediaQuery("(max-height: 750px)");
+  const [isWiderThan820px] = useMediaQuery("(min-width: 820px)");
 
   return (
     <Flex
@@ -14,16 +67,77 @@ const MalpunWelcome = () => {
         md: "calc(100vh - 4rem)",
         lg: "calc(100vh - 5rem)",
       }}
-      padding={{
-        base: "1rem",
-        md: "2rem",
-      }}
       overflow="hidden"
       bgColor="#1C3261"
       alignItems="center"
-      justifyContent="space-between"
-    ></Flex>
+      justifyContent="center"
+    >
+      <motion.div
+        className="motion-container"
+        initial="rest"
+        animate="enter"
+        exit="exit"
+        variants={cardVariants}
+      >
+        <Flex
+          h={{ base: "35%", sm: "40%", md: "50%" }}
+          justifyContent="flex-start"
+          className="mp-welcome-maskot"
+        >
+          <Image title="MAXI" src={MalpunMaxi} h="100%" w="auto" />
+          <Image
+            ml={{ base: "1rem", sm: "2rem", md: "3rem" }}
+            title="XIMA"
+            src={MalpunXima}
+            h="100%"
+            w="auto"
+          />
+        </Flex>
+        <Box className="mp-welcome-content-body">
+          <p>
+            "Selamat karena kamu hampir menyelesaikan perjalanan bersama{" "}
+            <strong>MAXIMA 2021</strong>. Pasti kamu lelah ya? Tenang saja, aku
+            akan mengajak kamu ketempat yang seru dan bisa bersenang-senang
+            sembari melepas lelah di sana!"
+          </p>
+        </Box>
+        <Flex
+          w="100%"
+          justifyContent={{ base: "center", md: "flex-end" }}
+          paddingRight={{ base: "0", md: "2rem", xl: "5rem" }}
+        >
+          <motion.div
+            variants={footerVariants}
+            initial="exit"
+            animate="enter"
+            exit="exit"
+          >
+            <Link to="/malpun/welcome">
+              <MxmButton colorScheme="red-red" padding="1.5rem">
+                <Text
+                  fontStyle="Rubik"
+                  fontWeight="500"
+                  fontSize={{ base: "1.2rem", md: "2rem" }}
+                >
+                  Berikutnya
+                </Text>
+                <NextIcon
+                  ml="1rem"
+                  boxSize={{ base: "0.7rem", md: "1.5rem" }}
+                  color="white"
+                />
+              </MxmButton>
+            </Link>
+          </motion.div>
+        </Flex>
+      </motion.div>
+    </Flex>
   );
 };
 
 export default MalpunWelcome;
+
+const NextIcon = createIcon({
+  viewBox: "0 0 88 88",
+  d: "M14.4721 1.23605C7.82311 -2.08846 0 2.74651 0 10.1803V77.8196C0 85.2535 7.82312 90.0884 14.4721 86.7639L82.1115 52.9443C89.482 49.259 89.4819 38.741 82.1115 35.0557L14.4721 1.23605Z",
+});
